0f4dda0c032ebcb894d07332485fff0e7aacde41
LeMasters <jacksonlemasters@outlook.com>
2022-01-01 17:33:36 -0700

    tailwind back working i dont even know

--- client/src/ToDoList.js
+++ client/src/ToDoList.js
@@ -8,15 +8,16 @@ class TodoList extends Component {
     return (
         <div className="todoListMain">
         <div className="text-3xl font-bold underline">Test</div>  
-        <div className="header">
+        <div className="flex flex-row">
             <form onSubmit={this.props.addItem}>
             <input
+                className="p-4 border-2 border-sky-500 w-48 text-base h-14"
                 placeholder="Task"
                 ref={this.props.inputElement}
                 value={this.props.currentItem.text}
                 onChange={this.props.handleInput}
             />
-            <button type="submit"> Add Task </button>
+            <button className="p-2 border-2 border-red-700 bg-red-500 w-24 h-14" type="submit"> Add Task </button>
             </form>
         </div>
         </div>
