0719f8a31dfb49552494236d66513fe50d0f012a
LeMasters <jacksonlemasters@outlook.com>
2022-01-02 23:03:38 -0700

    added some more tailwind

--- client/src/ToDoList.js
+++ client/src/ToDoList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+
 class TodoList extends Component {
     componentDidUpdate() {
     this.props.inputElement.current.focus()
@@ -11,13 +12,13 @@ class TodoList extends Component {
             <ul className="flex flex-grid m-4 font-bold text-lg">
                 <h1 className="pr-96">React toDo App</h1>
                 <li className="pr-12 pl-96"><a href="#home">Home</a></li>
-                <li className="pr-12"><a href="#Login">Login</a></li>
+                <li className="pr-12"><a href="">Login</a></li>
             </ul>
         </nav>  
-        <div className="flex flex-row">
+        <div className="flex flex-row mb-4 sm:w-1/2 md:w-full lg:w-1/4">
             <form onSubmit={this.props.addItem}>
             <input
-                className="p-4 border-2 border-sky-500 w-48 text-base h-14 mb-4"
+                className="p-4 border-2 border-sky-500 w-80 text-base h-14 "
                 placeholder="Task"
                 ref={this.props.inputElement}
                 value={this.props.currentItem.text}
